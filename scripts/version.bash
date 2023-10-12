46ed63fa5014404fe02429c9d314eb38552b3040
paulAlexSerban <paul.alex.serban@gmail.com>
2023-10-12 13:38:36 +0300

    chore(release): publish

---
+++ scripts/version.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+yarn --cwd .. lerna version --no-push --yes
+
+# Add changes to the staging area
+git add .
+
+# Commit the changes
+git commit -m "chore(release): publish"
+
+# Lastly, you might push tags manually if desired
+git push --tags
