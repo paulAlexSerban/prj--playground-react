b6328287af6e7cecc99d924d8bc15ea349ad51b1
paulAlexSerban <paul.alex.serban@gmail.com>
2023-11-14 20:41:26 +0200

    feat: mvp blogpost gallery

--- frontend/apps/blogpost-gallery-app/src/App.tsx
+++ frontend/apps/blogpost-gallery-app/src/App.tsx
@@ -1,30 +1,65 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useEffect, useState, type ReactNode } from 'react';
+import { get } from './util/http';
+import BlogPosts, { type BlogPost } from './components/BlogPosts';
+import ErrorMessage from './components/ErrorMessage';
+import fetchingImg from './assets/data-fetching.png';
+import './index.css';
+type RawDataBlogPost = {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+};
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>([]);
+    const [error, setError] = useState<string>();
+    const [isFetching, setIsFetching] = useState<boolean>(false);
 
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setIsFetching(true);
+            try {
+                const data = await get<RawDataBlogPost[]>('https://jsonplaceholder.typicode.com/posts');
+                if (data instanceof Error) {
+                    setError(data.message);
+                    return;
+                }
+                const blogPosts: BlogPost[] = data.map((post) => ({
+                    id: post.id,
+                    title: post.title,
+                    text: post.body,
+                }));
+                setFetchedPosts(blogPosts);
+            } catch (error) {
+                if (error instanceof Error) {
+                    setError(error.message);
+                }
+                return;
+            }
+
+            setIsFetching(false);
+        };
+
+        fetchPosts();
+    }, []);
+
+    let content: ReactNode;
+
+    if (error) {
+        content = <ErrorMessage text={error} />;
+    }
+    if (isFetching) {
+        content = <p id="loading-fallback">Loading posts...</p>;
+    }
+    if (fetchedPosts) {
+        content = <BlogPosts posts={fetchedPosts} />;
+    }
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <main>
+            <img src={fetchingImg} alt="Fetching data" />
+            {content}
+        </main>
     );
 }
 
