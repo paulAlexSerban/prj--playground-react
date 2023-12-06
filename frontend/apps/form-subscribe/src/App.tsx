c145bb5e23e6a3ad0f06b65aaaa55dfbf065072a
paulAlexSerban <paul.alex.serban@gmail.com>
2023-12-06 13:30:30 +0200

    feat: form submit - mvp

--- frontend/apps/form-subscribe/src/App.tsx
+++ frontend/apps/form-subscribe/src/App.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Input from './Input.tsx';
+import { useRef } from 'react';
+
+export const userData = {
+    name: '',
+    email: '',
+};
 
 function App() {
-    const [count, setCount] = useState(0);
+    const nameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+
+    function handleSaveData() {
+        if (!nameRef.current || !emailRef.current) {
+            return;
+        }
+        userData.name = nameRef.current.value;
+        userData.email = emailRef.current.value;
+
+        console.log(userData);
+    }
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <div id="app">
+            <Input ref={nameRef} type="text" label="Your Name" />
+            <Input ref={emailRef} type="email" label="Your E-Mail" />
+            <p id="actions">
+                <button onClick={handleSaveData}>Save Data</button>
+            </p>
+        </div>
     );
 }
 
