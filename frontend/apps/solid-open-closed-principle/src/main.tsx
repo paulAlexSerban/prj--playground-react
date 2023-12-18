5f6163f8140ab7ee514e38b7c7d68df1cf9c3020
paulAlexSerban <paul.alex.serban@gmail.com>
2023-12-18 12:13:12 +0200

    feat(refactor and ocp): added

--- frontend/apps/solid-open-closed-principle/src/main.tsx
+++ frontend/apps/solid-open-closed-principle/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ToDoPage from './TodoPage.tsx';
+import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ToDoPage />
+        <App />
     </React.StrictMode>
 );
